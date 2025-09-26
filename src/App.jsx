92e55055264b3d997addbe769cd8ffd33892e8e5
Igor Bakhtin <50614902+igor-vgs@users.noreply.github.com>
2025-09-26 17:09:19 +0300

    feat: add markup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 const API_KEY = "1f09ba2c556c86dg7afadcd2b05fe003"; //example, not a real key
 const BASE_GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?";
-const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 function App() {
-  const [location, setLocation] = useState("London");
-  const [forecast, setForecast] = useState({});
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState({});
 
   async function handleClick() {
     const geoApiUrl = `${BASE_GEO_URL}q=${location}&limit=1&appid=${API_KEY}`;
@@ -16,33 +16,68 @@ function App() {
         return { lat: data[0].lat, lon: data[0].lon };
       });
 
-    const weatherApiUrl = `${BASE_WEATHER_URL}lon=${geo.lon}&lat=${geo.lat}&appid=${API_KEY}&units=metric`;
+    const weatherApiUrl = `${BASE_URL}/weather?lon=${geo.lon}&lat=${geo.lat}&appid=${API_KEY}&units=metric`;
     fetch(weatherApiUrl)
       .then((response) => response.json())
-      .then((data) => setForecast(data));
+      .then((data) => setWeather(data));
   }
 
   return (
     <>
       <header>
-        <form>
-          <search>
-            <label>
-              Your City:
-              <input
-                type="search"
-                onChange={(e) => setLocation(e.target.value)}
-              />
-            </label>
-            <button onClick={handleClick}>Search</button>
-          </search>
-        </form>
+        <label>
+          Your City:
+          <input type="search" onChange={(e) => setLocation(e.target.value)} />
+        </label>
+        <button onClick={handleClick}>Search</button>
       </header>
-      <main>
-        Temperature: {Object.hasOwn(forecast, "main") && forecast.main.temp}
-        Feels like:{" "}
-        {Object.hasOwn(forecast, "main") && forecast.main.feels_like}
-      </main>
+      {Object.keys(weather).length ? (
+        <main>
+          <article>
+            <header>Date</header>
+            <p>Temperature: {weather.main.temp}</p>
+            <footer>Feels like: {weather.main.feels_like}</footer>
+          </article>
+          <article>
+            <ul>
+              <li>
+                <button>Today</button>
+              </li>
+              <li>
+                <button>Tomorrow</button>
+              </li>
+              <li>
+                <button>5 days</button>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <article>
+                  <header>Weekday, time</header>
+                  <p>Temperature</p>
+                  <footer>Wind</footer>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <header>Weekday, time</header>
+                  <p>Temperature</p>
+                  <footer>Wind</footer>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <header>Weekday, time</header>
+                  <p>Temperature</p>
+                  <footer>Wind</footer>
+                </article>
+              </li>
+            </ul>
+          </article>
+        </main>
+      ) : (
+        ""
+      )}
       <footer>
         My Github{" "}
         <a href="https://github.com/igor-bakhtin-gh">@igor-bakhtin-gh</a>
